3db8443b7fec1d0cbef06d48defd9292e23ba021
Stuart Lyu <stuazt@gmail.com>
2019-10-31 17:57:02 +0800

    Check resultCode before everything to avoid NPE.

--- android/src/main/java/com/peerwaya/flutteraccountkit/LoginResultDelegate.java
+++ android/src/main/java/com/peerwaya/flutteraccountkit/LoginResultDelegate.java
@@ -30,10 +30,15 @@ class LoginResultDelegate implements PluginRegistry.ActivityResultListener {
     @Override
     public boolean onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == FlutterAccountKitPlugin.APP_REQUEST_CODE) {
+            if (RESULT_OK != resultCode) {
+                finishWithResult(LoginResults.cancelledByUser);
+                return true;
+            }
+            
             AccountKitLoginResult loginResult = data.getParcelableExtra(AccountKitLoginResult.RESULT_KEY);
             if (loginResult.getError() != null) {
                 finishWithResult(LoginResults.error(loginResult.getError()));
-            } else if (RESULT_OK != resultCode || loginResult.wasCancelled()) {
+            } else if (loginResult.wasCancelled()) {
                 finishWithResult(LoginResults.cancelledByUser);
             } else {
                 finishWithResult(LoginResults.success(loginResult));
